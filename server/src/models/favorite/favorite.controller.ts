02adcd06c31427c548cbfdf6ab787010a29b6de5
Sergey Murin <sergeimurin29@gmail.com>
2023-02-06 15:04:32 +0300

    getFavorite &  getFavorites

--- server/src/models/favorite/favorite.controller.ts
+++ server/src/models/favorite/favorite.controller.ts
@@ -14,10 +14,28 @@ import { FavoriteService } from './favorite.service';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 import { RemoveFavoriteDto } from './dto/remove.favorite.dto';
+import { GetFavoritesDto } from './dto/get.favorites.dto';
 
 @Controller('favorite')
 export class FavoriteController {
   constructor(private readonly favoriteService: FavoriteService) {}
+
+  @Get()
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @UsePipes(new ValidationPipe())
+  async getFavorite(@Req() request: Request, @Body() dto: RemoveFavoriteDto) {
+    const token = request.headers.authorization.replace('Bearer ', '');
+    return await this.favoriteService.getFavorite(token, dto);
+  }
+
+  @Get('/all')
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @UsePipes(new ValidationPipe())
+  async getFavorites(@Body() dto: GetFavoritesDto) {
+    return await this.favoriteService.getFavorites(dto);
+  }
 
   @Delete()
   @ApiBearerAuth()
--- server/src/models/favorite/favorite.service.ts
+++ server/src/models/favorite/favorite.service.ts
@@ -12,6 +12,8 @@ import { AuthService } from '../user/auth/auth.service';
 import { FavoriteTrack } from './favorite.track/favorite.track.entity';
 import { FavoriteAlbum } from './favorite.album/favorite.album.entity';
 import { UserService } from '../user/user.service';
+import { GetFavoriteDto } from './dto/get.favorite.dto';
+import { GetFavoritesDto } from './dto/get.favorites.dto';
 
 @Injectable()
 export class FavoriteService {
@@ -26,9 +28,86 @@ export class FavoriteService {
     private readonly userService: UserService,
   ) {}
 
+  async getFavorite(token: string, dto: GetFavoriteDto) {
+    const jwtPayload = await this.authService.verifyToken(token);
+
+    const favorite = await this.favoriteRepository.findOne({
+      where: {
+        id: dto.favorite_id,
+      },
+    });
+
+    if (!favorite) {
+      throw new BadRequestException();
+    }
+
+    const favoriteTrack = await this.favoriteTrackRepository.findByPk(
+      favorite.id,
+    );
+    if (favoriteTrack) {
+      return {
+        type: 'track',
+        id: favoriteTrack.id,
+        user_id: favorite.user_id,
+        track_id: favoriteTrack.track_id,
+      };
+    }
+
+    const favoriteAlbum = await this.favoriteAlbumRepository.findByPk(
+      favorite.id,
+    );
+    if (favoriteAlbum) {
+      return {
+        type: 'album',
+        id: favoriteAlbum.id,
+        user_id: favorite.user_id,
+        track_id: favoriteAlbum.album_id,
+      };
+    }
+  }
+
+  async getFavorites(dto: GetFavoritesDto) {
+    const favorites = await this.favoriteRepository.findAll({
+      where: { user_id: dto.user_id },
+      order: [['updatedAt', 'ASC']],
+    });
+    if (!favorites) {
+      return [];
+    }
+
+    return Promise.all(
+      favorites.map(async (favorite) => {
+        const favoriteTrack = await this.favoriteTrackRepository.findOne({
+          where: { id: favorite.id },
+        });
+        if (favoriteTrack) {
+          return {
+            type: 'track',
+            id: favorite.id,
+            track_id: favoriteTrack.track_id,
+            user_id: dto.user_id,
+          };
+        }
+
+        const favoriteAlbum = await this.favoriteAlbumRepository.findOne({
+          where: { id: favorite.id },
+        });
+        if (favoriteAlbum) {
+          return {
+            type: 'album',
+            id: favorite.id,
+            track_id: favoriteAlbum.album_id,
+            user_id: dto.user_id,
+          };
+        }
+      }),
+    );
+  }
+
   async remove(token: string, dto: RemoveFavoriteDto) {
     const jwtPayload = await this.authService.verifyToken(token);
     const user = await this.userService.getById(jwtPayload.user_id);
+
     const favorite = await this.favoriteRepository.findOne({
       where: {
         user_id: user.id,
